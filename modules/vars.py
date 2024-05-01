30cead396b42814fcd6814619d92268fcc58fa7a
shuklapanditnakul <168512586+shuklapanditnakul@users.noreply.github.com>
2024-05-01 08:50:45 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "22800159"
 api_hash = "8dfbd34c757c84c887b58f6fd874f321"
-bot_token = "6851459979:AAFofn-uJHFL1eMklh-GTqPB82Rd5CQ2iR8"
+bot_token = "7027449492:AAF8_yeQPImCpg1o-XMv5RE7hmXUKbRPvYQ"
